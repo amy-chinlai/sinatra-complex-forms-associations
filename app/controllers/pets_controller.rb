9c0bc64357173bbbb674dac8ef8ccbfb0d7c3fdf
amy-chinlai <amy@actionnetwork.org>
2021-09-20 11:34:29 -0400

    commit tree f67f0de15ec6912fbdeb587ea64c72e57c64c407

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,18 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.create(:name => params[:pet_name])
+    if params[:pet][:owner_id].present?
+      @pet.owner = Owner.find_by_id(params[:pet][:owner_id])
+    else
+      @pet.owner = Owner.create(:name => params[:pet][:owner_name])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 
@@ -20,7 +27,23 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do 
-
+    @pet = Pet.find(params[:id])
+    @pet.name = params[:pet_name]
+    # binding.pry
+    if params[:owner][:name].present?
+      @pet.owner = Owner.create(:name => params[:owner][:name])
+    else
+      @pet.owner = Owner.find(params[:pet][:owner_id])
+    end
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
+
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find(params[:id])
+    erb :'pets/edit'
+  end
+
+
 end
